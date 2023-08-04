ec5ed0d64b6e8b6362f4bd7785d19a283f2f43ec
Richard William <rwilliam.flores@gmail.com>
2023-08-04 19:38:02 +0800

    added update post functionality

--- app/api/posts/[id]/route.ts
+++ app/api/posts/[id]/route.ts
@@ -12,3 +12,19 @@ export const DELETE = async (_: NextRequest, { params }: { params: RequestParams
 
     return NextResponse.json({ data: post });
 };
+
+export const PATCH = async (req: NextRequest, { params }: { params: RequestParams }) => {
+    const { caption } = await req.json();
+    const { id } = params;
+
+    const updatedPost = await prisma.post.update({
+        where: {
+            id,
+        },
+        data: {
+            caption,
+        },
+    });
+
+    return NextResponse.json({ data: updatedPost });
+};
