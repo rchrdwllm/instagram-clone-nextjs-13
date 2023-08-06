4cc3a774f8d1cde8be31e2959ac64b05609936e0
Richard William <rwilliam.flores@gmail.com>
2023-08-06 10:58:56 +0800

    updated breakpoints

--- app/(user)/feed/loading.tsx
+++ app/(user)/feed/loading.tsx
@@ -2,7 +2,7 @@ import SkeletonPost from '@/components/SkeletonPost';
 
 const Loading = () => {
     return (
-        <div className="h-screen flex flex-col space-y-8 pt-8 px-56 pb-20">
+        <div className="h-screen flex flex-col space-y-8 pt-8 px-72 pb-20 2xl:px-[28rem] xl:px-80 lg:px-56">
             <SkeletonPost />
             <SkeletonPost />
         </div>
