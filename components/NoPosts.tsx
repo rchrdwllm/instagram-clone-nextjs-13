d5383f5473b65e3d7c5ace991d44ca7de9d1703b
Richard William <rwilliam.flores@gmail.com>
2023-08-06 10:53:37 +0800

    updated text

--- components/NoPosts.tsx
+++ components/NoPosts.tsx
@@ -1,7 +1,7 @@
 const NoPosts = () => {
     return (
         <div className="h-full w-full flex items-center justify-center">
-            <p className="text-slate-600 text-sm">This user hasn't posted anything yet</p>
+            <p className="text-slate-600 text-sm">This user hasn't posted anything yet!</p>
         </div>
     );
 };
